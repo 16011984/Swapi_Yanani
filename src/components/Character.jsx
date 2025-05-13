235cd20017d528903581746f62d056d08c2f1c5f
Dani Yanani <alejandrorot2@gmail.com>
2025-05-13 22:57:11 +0000

    en marcha

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,19 +1,23 @@
 import "./character.css";
-
+import CharacterCard from "./Charactercard";
 
 const Character = () => {
 
+        return (
+                <div className="character">
 
- return(
-         <div className="cha-container">
+                        <div className="cha-btn">
+                                <button className="cha-btn-next"> Next </button>
+                                <button className="cha-btn-back"> Back </button>
+                        </div>
 
-            <div className="cha-card">
+                        <div className="cha-cha-card">
+                                <CharacterCard />
+                        </div>
 
-            </div>
-        
-        </div>
+                </div>
 
-        ); 
+        );
 }
 
 export default Character;
---
+++ src/components/Charactercard.jsx
@@ -0,0 +1,17 @@
+import "./charactercard.css";
+
+const CharacterCard = () => {
+
+return(
+
+        <div className="cha_card">
+            
+        </div>
+
+
+       )
+
+
+};
+
+export default CharacterCard;
